79079c0b71e75802f54f26fcd1cc36628dac2791
Gokul R <gokul@sarva.ai>
2023-04-29 16:38:41 +0530

    chore: remove wallet and signer packages from moi

--- packages/moi/dist/index.d.ts
+++ packages/moi/dist/index.d.ts
@@ -1,7 +1,5 @@
 export * from "moi-providers";
 export * from "moi-abi";
 export * from "moi-logic";
-export * from "moi-signer";
-export * from "moi-wallet";
 export * from "moi-constants";
 export * from "moi-utils";
--- packages/moi/dist/index.js
+++ packages/moi/dist/index.js
@@ -17,7 +17,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("moi-providers"), exports);
 __exportStar(require("moi-abi"), exports);
 __exportStar(require("moi-logic"), exports);
-__exportStar(require("moi-signer"), exports);
-__exportStar(require("moi-wallet"), exports);
 __exportStar(require("moi-constants"), exports);
 __exportStar(require("moi-utils"), exports);
