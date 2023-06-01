9a88eaa75b7efe1ef4d8b50fd92f52adaa708d4e
sarvalabs-sai <sai@sarva.ai>
2023-06-02 01:43:08 +0530

    fix: signature verification irrespective of parity byte in publicKey

--- packages/moi-signer/src/ecdsa.ts
+++ packages/moi-signer/src/ecdsa.ts
@@ -1,17 +1,19 @@
 import { Wallet } from "moi-wallet";
 import { SigType } from "../types";
+import Signature from "./signature";
 import { ECPair, script, Transaction, networks } from "bitcoinjs-lib";
 import Blake2b from "blake2b";
 
 export default class ECDSA_S256 implements SigType {
     prefix: number;
     sigName: string;
+
     constructor() {
         this.prefix = 1;
         this.sigName = "ECDSA_S256";
     }
 
-    sign(message: Buffer, vault: Wallet): String {
+    sign(message: Buffer, vault: Wallet): Signature {
         let signingKey = vault.privateKey();
         let _signingKey: Buffer
         if(typeof signingKey === "string") {
@@ -36,17 +38,23 @@ export default class ECDSA_S256 implements SigType {
         prefixArray[0] = this.prefix;
         prefixArray[1] = signature.length;
 
-        const finalSigBytes = Buffer.concat([Buffer.from(prefixArray), signature]);
-        return finalSigBytes.toString('hex');
+        const sig = new Signature(Buffer.from(prefixArray), signature, Buffer.from([keyPair.publicKey[0]]), this.sigName);
+        return sig;
     }
-    verify(message: Buffer, signature: Buffer, publicKey: Buffer): Boolean {        
+
+    verify(message: Buffer, signature: Signature, publicKey: Buffer): Boolean {  
+
+        let verificationKey = Buffer.concat([signature.Extra(), publicKey])
+
+        let rawSignature = signature.Digest();
+        // Appending the byte that was removed at the time of signing      
         let sigComps = script.signature.decode(
-            Buffer.concat([signature, Buffer.from([0x01])])
+            Buffer.concat([rawSignature, Buffer.from([0x01])])
         );
 
         // Hashing raw message with blake2b to get 32 bytes digest 
         const messageHash = Blake2b(256 / 8).update(message).digest();
-        const parsedPubkey = ECPair.fromPublicKey(publicKey, { network: networks.bitcoin });
+        const parsedPubkey = ECPair.fromPublicKey(verificationKey, { network: networks.bitcoin });
 
         return parsedPubkey.verify(messageHash, sigComps.signature)
     }
