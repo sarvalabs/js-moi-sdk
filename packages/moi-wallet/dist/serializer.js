f6ed2709c61bbcccefdc1d05902d6da6982b7cd4
Gokul R <gokul@sarva.ai>
2023-06-20 22:19:47 +0530

    refactor: asset create schema and interaface changes build

--- packages/moi-wallet/dist/serializer.js
+++ packages/moi-wallet/dist/serializer.js
@@ -74,9 +74,6 @@ const logicSchema = {
 const assetCreateSchema = {
     kind: "struct",
     fields: {
-        type: {
-            kind: "integer"
-        },
         symbol: {
             kind: "string"
         },
