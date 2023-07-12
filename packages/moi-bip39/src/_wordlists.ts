4e68fcf4e87e6d87bcaf497df5ea08897aa29e44
Sai Prasanth Vuppala <96536302+sarvalabs-sai@users.noreply.github.com>
2023-07-13 02:28:46 +0530

    docs: copyright attribution for moi-bip39 package (#7)

--- packages/moi-bip39/src/_wordlists.ts
+++ packages/moi-bip39/src/_wordlists.ts
@@ -1,3 +1,15 @@
+/**
+ * This code is based on the bitcoinjs/bip39 by Wei Lu and Danieal Cousens
+ * Modifications have been made to adapt it to the needs of moi-bip39 
+ * including enhancements for browser compatibility and TypeScript conversion.
+ * 
+ * Original module available at: https://github.com/bitcoinjs/bip39
+ * Modified version available at: https://github.com/sarvalabs/moi.js/tree/main/packages/moi-bip39
+ * 
+ * Copyright (c) 2014, Wei Lu <luwei.here@gmail.com> and Daniel Cousens <email@dcousens.com>
+ * Repository ISC license details can be found at https://github.com/bitcoinjs/bip39/blob/master/LICENSE
+ * 
+ **/
 import czech from './wordlists/czech.json';
 import chinese_simplified from './wordlists/chinese_simplified.json';
 import chinese_traditional from './wordlists/chinese_traditional.json';
