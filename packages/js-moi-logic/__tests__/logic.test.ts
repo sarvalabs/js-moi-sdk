1ab415d97684c22cd90dc6099fc38c655ee7e9c3
Harsh Rastogi <harsh.rastogi@sarva.ai>
2024-01-21 18:04:38 +0530

    fix: update typo

--- packages/js-moi-logic/__tests__/logic.test.ts
+++ packages/js-moi-logic/__tests__/logic.test.ts
@@ -18,7 +18,7 @@ describe("Test Logic Deploy", () => {
 
     test.concurrent("should deploy the Todo contract with routine request options", async() => {
         const provider = initializeProvider();
-        const mnemonic = "crisp seed misery heart hire record can lab exchange skirt always that"
+        const mnemonic = "crisp seed misery heart hire cat can lab exchange skirt always mice"
         const wallet  = await initializeWallet(provider, mnemonic);
 
         const factory = new LogicFactory(todo, wallet);
@@ -38,7 +38,7 @@ describe("Test Logic Deploy", () => {
 
     test.concurrent("should deploy the todo routine with routine request options", async() => {
         const provider = initializeProvider();
-        const mnemonic = "crisp seed misery heart hire record can lab exchange skirt always that"
+        const mnemonic = "crisp seed misery heart hire cat can lab exchange skirt always mice"
         const wallet  = await initializeWallet(provider, mnemonic);
 
         console.log(wallet.getAddress())
@@ -62,7 +62,7 @@ describe("Test Logic Deploy", () => {
     test.concurrent("show able to insert add without routine options", async() => {
         const logicId = "0x080000c613b3d1ec878c2879f9e122e795733bb7e98298af3080256466ab133407fce6"
         const provider = initializeProvider();
-        const mnemonic = "crisp seed misery heart hire record can lab exchange skirt always that"
+        const mnemonic = "crisp seed misery heart hire cat can lab exchange skirt always mice"
         const wallet  = await initializeWallet(provider, mnemonic);
 
         const logicDriver = await getLogicDriver(logicId, wallet);
@@ -80,7 +80,7 @@ describe("Test Logic Deploy", () => {
     test.concurrent("show able to retrieve the todo list", async () => {
         const logicId = "0x0800004bf40852ac85851a2f75a657e4bd2b35d753d5ccc1d904c93733511ae2c3111a"
         const provider = initializeProvider();
-        const mnemonic = "crisp seed misery heart hire record can lab exchange skirt always that"
+        const mnemonic = "crisp seed misery heart hire cat can lab exchange skirt always mice"
         const wallet  = await initializeWallet(provider, mnemonic);
 
         const logicDriver = await getLogicDriver(logicId, wallet);
@@ -93,7 +93,7 @@ describe("Test Logic Deploy", () => {
     test.concurrent("show able to retrieve the todo list with routine options", async () => {
         const logicId = "0x0800004bf40852ac85851a2f75a657e4bd2b35d753d5ccc1d904c93733511ae2c3111a"
         const provider = initializeProvider();
-        const mnemonic = "crisp seed misery heart hire record can lab exchange skirt always that"
+        const mnemonic = "crisp seed misery heart hire cat can lab exchange skirt always mice"
         const wallet  = await initializeWallet(provider, mnemonic);
 
         const logicDriver = await getLogicDriver(logicId, wallet);
