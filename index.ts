0d1497b0189731a87256aeb44509cafdfbbd1693
Harsh Rastogi <harsh.rastogi@sarva.ai>
2024-01-21 20:16:05 +0530

    chore: remove unwanted file

--- index.ts
+++
@@ -1,5 +0,0 @@
-import { getLogicDriver } from "./packages/js-moi-logic";
-import { Wallet } from "./packages/js-moi-wallet";
-
-const wallet = new Wallet()
-const logicDriver = getLogicDriver("0x0000000", wallet);
