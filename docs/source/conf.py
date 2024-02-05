309c97ec12e987d4064ce05b0b426477c3b70f63
Harsh Rastogi <harsh.rastogi@sarva.ai>
2024-02-05 15:39:57 +0530

    chore: bump version numbers to 0.2.1-rc1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,9 @@ author = 'Sarva Labs Inc'
 # built documents.
 #
 # The short X.Y version.
-version = u'v0.2.0'
+version = u'v0.2.1-rc1'
 # The full version, including alpha/beta/rc tags.
-release = u'v0.2.0'
+release = u'v0.2.1-rc1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- packages/js-moi-constants/dist/version.js
+++ packages/js-moi-constants/dist/version.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VERSION = void 0;
-exports.VERSION = "0.2.0";
+exports.VERSION = "0.2.1-rc1";
