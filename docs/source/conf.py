9b72ef9d6f7653b3e189af7037b4770468931135
Harsh Rastogi <harsh.rastogi@sarva.ai>
2024-02-17 00:58:54 +0530

    chore: bump pkg version to v0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,9 @@ author = 'Sarva Labs Inc'
 # built documents.
 #
 # The short X.Y version.
-version = u'v0.2.1-rc2'
+version = u'v0.2.1'
 # The full version, including alpha/beta/rc tags.
-release = u'v0.2.1-rc2'
+release = u'v0.2.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
