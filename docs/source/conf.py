53c1889da769e1986e5caa023ace4aa93b05ac46
Harsh Rastogi <142212174+sarvalabs-harshrastogi@users.noreply.github.com>
2024-02-19 12:47:24 +0530

    Implemented v0.3.0-rc2 changes (#50)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,9 @@ author = 'Sarva Labs Inc'
 # built documents.
 #
 # The short X.Y version.
-version = u'v0.3.0-rc1'
+version = u'v0.3.0-rc2'
 # The full version, including alpha/beta/rc tags.
-release = u'v0.3.0-rc1'
+release = u'v0.3.0-rc2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
