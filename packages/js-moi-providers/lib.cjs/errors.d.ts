4068d12351429d66a8c9b7f128c529c463717161
Harsh Rastogi <harsh.rastogi@sarva.ai>
2024-03-07 13:43:06 +0530

    refactor: js-moi-provider builds

---
+++ packages/js-moi-providers/lib.cjs/errors.d.ts
@@ -0,0 +1,5 @@
+export declare const ConnectionNotOpenError: (event?: any) => Error;
+export declare const InvalidConnection: (host: string, event: any) => Error;
+export declare const MaxAttemptsReachedOnReconnectingError: () => Error;
+export declare const PendingRequestsOnReconnectingError: () => Error;
+//# sourceMappingURL=errors.d.ts.map
