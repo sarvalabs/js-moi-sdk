96e5311872d3db64cbde13ca7ae2d3aba4de0f80
Harsh Rastogi <142212174+sarvalabs-harshrastogi@users.noreply.github.com>
2024-04-30 04:07:30 +0530

    chore: bump the package to v0.4.0 (#66)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,9 @@ author = 'Sarva Labs Inc'
 # built documents.
 #
 # The short X.Y version.
-version = u'v0.3.0-rc4'
+version = u'v0.4.0'
 # The full version, including alpha/beta/rc tags.
-release = u'v0.3.0-rc4'
+release = u'v0.4.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- packages/js-moi-constants/src.ts/version.ts
+++ packages/js-moi-constants/src.ts/version.ts
@@ -1,1 +1,1 @@
-export const VERSION = "0.3.0-rc4";
+export const VERSION = "0.4.0";
