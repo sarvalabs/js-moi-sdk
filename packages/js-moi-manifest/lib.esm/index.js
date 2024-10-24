6505b4cd2d78a782160c1023e105e7c3f0861bd8
Harsh Rastogi <harsh.rastogi@sarva.ai>
2024-10-24 17:34:28 +0530

    refactor: reorder export statements for consistency

--- packages/js-moi-manifest/lib.esm/index.js
+++ packages/js-moi-manifest/lib.esm/index.js
@@ -1,6 +1,6 @@
-export * from "./context-state-matrix";
-export * from "./element-descriptor";
 export * from "./manifest";
 export { ManifestFormat } from "./manifest-format";
+export * from "./context-state-matrix";
+export * from "./element-descriptor";
 export * from "./schema";
 //# sourceMappingURL=index.js.map
