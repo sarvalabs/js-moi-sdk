18386312549fcd592a23670c5e5ddf5dce441b29
Harsh Rastogi <harsh.rastogi@sarva.ai>
2024-10-28 14:07:00 +0530

    refactor: update test constants for clarity in websocket provider tests

--- packages/js-moi-providers/__tests__/ws-provider.test.ts
+++ packages/js-moi-providers/__tests__/ws-provider.test.ts
@@ -4,9 +4,9 @@ import { WebSocketEvent } from "../src.ts/websocket-events";
 import { WebsocketProvider } from "../src.ts/websocket-provider";
 import { getRandomSupply, initializeWallet } from "./utils/utils";
 
-const HOST = "<YOUR WEBSOCKET HOST PATH (ws://localhost:1600/ws)>";
-const MNEMONIC = "<YOUR SEED RECOVERY PHRASE>";
-const ASSET_ID = "0x00000000c67fbf5825d505586701135114fe76f5c0b18675c4a08467e8aa1495482c20c7";
+const HOST = "<YOUR WEBSOCKET HOST>";
+const MNEMONIC = "<YOUR MNEMONIC>";
+const ASSET_ID = "<YOUR ASSET ID>";
 
 describe("Test Websocket Provider", () => {
     const isValidMnemonic = MNEMONIC.split(" ").length === 12;
