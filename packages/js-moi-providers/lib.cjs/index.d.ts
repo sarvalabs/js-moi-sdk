099321f7ffa8fda363ecc7f082c979eb6ceb05c2
Harsh Rastogi <harsh.rastogi@sarva.ai>
2024-10-28 14:24:47 +0530

    fix: remove duplicate type exports in index files for cleaner module structure

--- packages/js-moi-providers/lib.cjs/index.d.ts
+++ packages/js-moi-providers/lib.cjs/index.d.ts
@@ -1,10 +1,10 @@
+export type * from "../types/event";
+export type * from "../types/jsonrpc";
+export type * from "../types/provider";
 export * from "./abstract-provider";
 export * from "./base-provider";
 export * from "./jsonrpc-provider";
 export * from "./voyage-provider";
-export type * from "../types/event";
-export type * from "../types/jsonrpc";
-export type * from "../types/provider";
 export { WebSocketEvent } from "./websocket-events";
 export { WebsocketProvider } from "./websocket-provider";
 //# sourceMappingURL=index.d.ts.map
