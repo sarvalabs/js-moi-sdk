60cf3e5dffff1617199af4d09ee828964e1958d1
Gokul R <gokul@sarva.ai>
2024-11-16 00:22:47 +0530

    release: bump the package version to v0.6.0 (#98)

--- packages/js-moi-constants/lib.cjs/version.js
+++ packages/js-moi-constants/lib.cjs/version.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VERSION = void 0;
-exports.VERSION = "0.5.0";
+exports.VERSION = "0.6.0";
 //# sourceMappingURL=version.js.map
