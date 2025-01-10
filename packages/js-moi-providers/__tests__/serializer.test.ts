59e052c9fefa286acddbd2e26ad40bd72a037bcb
Harsh Rastogi <harsh.rastogi@sarva.ai>
2025-01-10 15:39:43 +0530

    test: enable serialization test for participant create operation payload with AssetStandard

--- packages/js-moi-providers/__tests__/serializer.test.ts
+++ packages/js-moi-providers/__tests__/serializer.test.ts
@@ -1,4 +1,4 @@
-import { bytesToHex, OpType } from "js-moi-utils";
+import { AssetStandard, bytesToHex, OpType } from "js-moi-utils";
 import { InteractionSerializer, type IxOperation } from "../src.ts";
 
 const serializer = new InteractionSerializer();
@@ -11,18 +11,18 @@ interface TestCase {
 
 describe("Serialization of ix operation payload", () => {
     const tests: TestCase[] = [
-        // {
-        //     name: "should serialize a participant create operation payload",
-        //     payload: {
-        //         type: OpType.ASSET_CREATE,
-        //         payload: {
-        //             symbol: "MOI",
-        //             supply: 500,
-        //             standard: AssetStandard.MAS0,
-        //         },
-        //     },
-        //     expected: "0x0e7f063353535151504d4f4901f4",
-        // },
+        {
+            name: "should serialize a participant create operation payload",
+            payload: {
+                type: OpType.ASSET_CREATE,
+                payload: {
+                    symbol: "MOI",
+                    supply: 500,
+                    standard: AssetStandard.MAS0,
+                },
+            },
+            expected: "0x0e7f063353535151504d4f4901f4",
+        },
         {
             name: "should serialize a participant create operation payload",
             payload: {
