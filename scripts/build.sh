b4311c1da832da6179b542591400ca49cc6c913b
Harsh Rastogi <harsh.rastogi@sarva.ai>
2025-01-12 19:36:16 +0530

    feat: add package.json files for CommonJS and ESM modules in multiple packages and update build script for directory structure

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,31 @@
-# Clean all lib.* directories in packages/ and dist in root directory
+# Clean all lib.* directories in packages/*/ and dist in root directory
 
 echo "Cleaning all lib.* directories in packages/ and dist in root directory"
 
 rm -rf packages/*/lib.*
 rm -rf dist
+
+# Create lib.* directories in packages/* with file package.json such
+# if directory is `lib.esm` then file package.json will be created with
+# content `{"type": "module"}`
+
+echo "Building project distribution"
+
+for dir in packages/*; do
+    if [ -d "$dir" ]; then
+        echo "Building $dir"
+
+        mkdir -p $dir/lib.esm
+        echo '{"type": "module"}' >$dir/lib.esm/package.json
+
+        mkdir -p $dir/lib.cjs
+        echo '{"type": "commonjs"}' >$dir/lib.cjs/package.json
+
+    fi
+done
+
+# Run typescript build
+
+tsc -b
+
+echo "Project distribution built successfully 🎉"
