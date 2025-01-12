a95acf997f7a75af2e060b56fe453eb264f9d2dd
Harsh Rastogi <harsh.rastogi@sarva.ai>
2025-01-12 10:44:44 +0530

    feat: enhance TypeScript types for NetworkActionApi with MethodParams and MethodResponse

--- packages/js-moi-providers/src.ts/types/moi-execution-api.ts
+++ packages/js-moi-providers/src.ts/types/moi-execution-api.ts
@@ -9,8 +9,17 @@ export interface NetworkActionApi {
     "moi.Protocol": ApiMethod<[option?: ResponseModifierParam<keyof NetworkInfo>], any>;
 }
 
+/**
+ * The `NetworkMethod` type is used to extract the method names from the `NetworkActionApi`.
+ */
 export type NetworkMethod = keyof NetworkActionApi;
 
+/**
+ * The `MethodParams` type is used to extract the parameters of a method from the `NetworkActionApi`.
+ */
 export type MethodParams<T extends NetworkMethod> = T extends NetworkMethod ? NetworkActionApi[T]["params"] : any[];
 
+/**
+ * The `MethodResponse` type is used to extract the response of a method from the `NetworkActionApi`.
+ */
 export type MethodResponse<T extends string> = T extends NetworkMethod ? NetworkActionApi[T]["response"] : any;
