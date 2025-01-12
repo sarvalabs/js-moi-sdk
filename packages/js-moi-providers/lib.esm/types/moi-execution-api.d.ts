9ce218a0abefeedf7295800bf464c1f9417f8719
Harsh Rastogi <harsh.rastogi@sarva.ai>
2025-01-12 10:33:00 +0530

    feat: add TypeScript types for NetworkActionApi and related methods

---
+++ packages/js-moi-providers/lib.esm/types/moi-execution-api.d.ts
@@ -0,0 +1,12 @@
+import type { NetworkInfo, ResponseModifierParam } from "js-moi-utils";
+export type ApiMethod<TParams extends any[], TResponse> = {
+    params: TParams;
+    response: TResponse;
+};
+export interface NetworkActionApi {
+    "moi.Protocol": ApiMethod<[option?: ResponseModifierParam<keyof NetworkInfo>], any>;
+}
+export type NetworkMethod = keyof NetworkActionApi;
+export type MethodParams<T extends string> = T extends NetworkMethod ? NetworkActionApi[T]["params"] : any[];
+export type MethodResponse<T extends string> = T extends NetworkMethod ? NetworkActionApi[T]["response"] : any;
+//# sourceMappingURL=moi-execution-api.d.ts.map
