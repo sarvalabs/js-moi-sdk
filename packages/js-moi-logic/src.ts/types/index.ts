49c96c4965d145b822d2c16fd52ef55afd774c74
Harsh Rastogi <harsh.rastogi@sarva.ai>
2025-01-17 00:16:41 +0530

    refactor: simplify CallsiteCallback type definitions by introducing CallsiteResult

--- packages/js-moi-logic/src.ts/types/index.ts
+++ packages/js-moi-logic/src.ts/types/index.ts
@@ -12,25 +12,26 @@ export type CallsiteOption = {
     sequence?: number;
 };
 
+type CallsiteResult = InteractionResponse | Record<any, any>;
 // Typescript doesn't last param of type T to be optional
 // This is pretty the below code as it more readable
 // prettier-ignore
 export interface CallsiteCallback {
-    <TRes extends Record<any, any> | InteractionResponse>(option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, arg10: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, arg10: any, arg11: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, arg10: any, arg11: any, arg12: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, arg10: any, arg11: any, arg12: any, arg13: any, option?: CallsiteOption): Promise<TRes>;
-    <TRes extends Record<any, any> | InteractionResponse>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, arg10: any, arg11: any, arg12: any, arg13: any, arg14: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, arg10: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, arg10: any, arg11: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, arg10: any, arg11: any, arg12: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, arg10: any, arg11: any, arg12: any, arg13: any, option?: CallsiteOption): Promise<TRes>;
+    <TRes extends CallsiteResult = any>(arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any, arg8: any, arg9: any, arg10: any, arg11: any, arg12: any, arg13: any, arg14: any, option?: CallsiteOption): Promise<TRes>;
 }
 
 export type LogicCallsites = Record<string, CallsiteCallback>;
