73be3a4466f68b0c611122105266005f058c54f1
Harsh Rastogi <harsh.rastogi@sarva.ai>
2025-01-19 08:53:36 +0530

    test: enhance wallet tests with error handling for invalid keystore and password

--- packages/js-moi-wallet/__tests__/wallet.test.ts
+++ packages/js-moi-wallet/__tests__/wallet.test.ts
@@ -1,5 +1,5 @@
 import type { SigType } from "js-moi-signer";
-import { AssetStandard, hexToBytes, OpType, type InteractionRequest } from "js-moi-utils";
+import { AssetStandard, hexToBytes, isHex, OpType, type InteractionRequest } from "js-moi-utils";
 import { CURVE, Wallet } from "../src.ts";
 
 const MNEMONIC = "profit behave tribe dash diet stool crawl general country student smooth oxygen";
@@ -43,9 +43,10 @@ describe(Wallet, () => {
         expect(wallet.curve).toEqual(CURVE.SECP256K1);
     });
 
+    const keystore =
+        '{"cipher":"aes-128-ctr","ciphertext":"d2574897079cf82af2b48848cdc44ec40bd0d383562978eb50dd032e38b1c90e","cipherparams":{"IV":"5d98f0c4d8562244ee48d063a3d6ce07"},"kdf":"scrypt","kdfparams":{"n":4096,"r":8,"p":1,"dklen":32,"salt":"a99102406c29fdbc79793be3e4e4cae2bbc5c38a05a6e2917228fd6eea2244d9"},"mac":"33d1569f411526447edf1970cb5d61ed097543521024ebafbdd497c1017126f8"}';
+
     it.concurrent("should create a wallet using keystore and password", async () => {
-        const keystore =
-            '{"cipher":"aes-128-ctr","ciphertext":"d2574897079cf82af2b48848cdc44ec40bd0d383562978eb50dd032e38b1c90e","cipherparams":{"IV":"5d98f0c4d8562244ee48d063a3d6ce07"},"kdf":"scrypt","kdfparams":{"n":4096,"r":8,"p":1,"dklen":32,"salt":"a99102406c29fdbc79793be3e4e4cae2bbc5c38a05a6e2917228fd6eea2244d9"},"mac":"33d1569f411526447edf1970cb5d61ed097543521024ebafbdd497c1017126f8"}';
         const password = "password";
         const wallet = Wallet.fromKeystore(keystore, password);
 
@@ -54,6 +55,20 @@ describe(Wallet, () => {
         expect(wallet.privateKey).toEqual(PRIVATE_KEY);
         expect(wallet.mnemonic).not.toBeDefined();
         expect(wallet.curve).toEqual(CURVE.SECP256K1);
+    });
+
+    it.concurrent("should throw an error if password is incorrect", async () => {
+        const password = "wrong";
+        const createWallet = () => Wallet.fromKeystore(keystore, password);
+
+        expect(createWallet).toThrow();
+    });
+
+    it.concurrent("should throw an error if keystore is invalid", async () => {
+        const password = "password";
+        const createWallet = () => Wallet.fromKeystore("{}", password);
+
+        expect(createWallet).toThrow();
     });
 
     let wallet = Wallet.fromMnemonicSync(MNEMONIC, { path: DEVIATION_PATH });
@@ -127,12 +142,21 @@ describe(Wallet, () => {
             expect(signing).rejects.toThrow();
         });
 
+        it.concurrent("should throw an error if sender address is not the same as wallet address", async () => {
+            const ix: InteractionRequest = { ...interaction, sender: { ...interaction.sender, address: "0x123" } };
+            const signing = async () => await wallet.signInteraction(ix, algorithm);
+
+            expect(signing).rejects.toThrow();
+        });
+
         it.concurrent("should sign an interaction using ECDSA secp256k1", async () => {
             const { interaction: encoded, signatures } = await wallet.signInteraction(interaction, algorithm);
             const expectedSignature =
                 "0x0146304402201781a592e1543d7035c7e2bcc371054fb1b31329c8215990012a27049c802d9402201ed6ac66c334e037fa347a647e7e4a1c64926af67301d8deac71dbaddcf84d6f02";
             const verify = wallet.verify(hexToBytes(encoded), signatures[0].signature, wallet.publicKey);
 
+            expect(isHex(encoded)).toBeTruthy();
+            expect(signatures).toHaveLength(1);
             expect(signatures[0].signature).toEqual(expectedSignature);
             expect(verify).toBeTruthy();
         });
