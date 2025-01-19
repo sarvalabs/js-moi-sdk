f101b9ad0aa787ffebdafe555a7aeeae6f0ea7d2
Harsh Rastogi <harsh.rastogi@sarva.ai>
2025-01-19 14:30:04 +0530

    refactor: consolidate Jest configuration and improve package management for testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,92 @@
+import deepmerge from "deepmerge";
 import type { Config } from "jest";
+
+interface Package {
+    name: string;
+    run_tests: boolean;
+    config?: Config;
+}
+
+/**
+ * Retrieves an array of package configurations.
+ *
+ * Each package configuration includes the package name and a flag indicating
+ * whether tests should be run for that package.
+ *
+ * The `config` property can be used to provide a custom Jest configuration which
+ * will override the shared configuration.
+ *
+ * @returns {Package[]} An array of package configurations.
+ */
+const getPackages = (): Package[] => [
+    {
+        name: "js-moi-bip39",
+        run_tests: true,
+    },
+    {
+        name: "js-moi-hdnode",
+        run_tests: true,
+    },
+    {
+        name: "js-moi-manifest",
+        run_tests: true,
+    },
+    {
+        name: "js-moi-logic",
+        run_tests: false,
+    },
+    {
+        name: "js-moi-signer",
+        run_tests: true,
+    },
+    {
+        name: "js-moi-providers",
+        run_tests: false,
+    },
+    {
+        name: "js-moi-utils",
+        run_tests: true,
+    },
+    {
+        name: "js-moi-wallet",
+        run_tests: true,
+    },
+];
+
+const getSharedConfiguration = (pkg: Package): Config => {
+    return {
+        transform: { "^.+.tsx?$": ["ts-jest", {}] },
+        displayName: pkg.name,
+        testMatch: [`<rootDir>/packages/${pkg.name}/__tests__/*.test.ts`],
+    };
+};
+
+const getPackageConfiguration = (pkg: Package): Config => {
+    const shared: Config = getSharedConfiguration(pkg);
+    const config: Config = pkg.config ?? {};
+
+    return deepmerge.all<Config>([shared, config]);
+};
+
+const getProjects = () => {
+    return getPackages()
+        .filter((pkg) => pkg.run_tests)
+        .map((pkg) => getPackageConfiguration(pkg));
+};
 
 const config: Config = {
     clearMocks: true,
-    collectCoverage: false,
+    collectCoverage: true,
     coverageDirectory: "coverage",
+    coverageReporters: ["html", "text-summary"],
     coverageProvider: "v8",
-    projects: [
-        {
-            transform: { "^.+.tsx?$": ["ts-jest", {}] },
-            displayName: "js-moi-utils",
-            testEnvironment: "node",
-            testMatch: ["<rootDir>/packages/js-moi-utils/__tests__/*.test.ts"],
-        },
-    ],
+    maxConcurrency: 10,
+    detectLeaks: true,
+    verbose: true,
+    workerThreads: true,
+    maxWorkers: 10,
+    coveragePathIgnorePatterns: ["<rootDir>/packages/*/__tests__/.+"],
+    projects: getProjects(),
 };
 
 export default config;
