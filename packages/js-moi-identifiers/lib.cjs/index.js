067f74bc3ae2d85526c6c40251ce11adbf01f5fe
Harsh Rastogi <harsh.rastogi@sarva.ai>
2025-01-27 19:42:45 +0530

    feat: update exports in index files to remove unused methods and improve clarity

--- packages/js-moi-identifiers/lib.cjs/index.js
+++ packages/js-moi-identifiers/lib.cjs/index.js
@@ -1,12 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ParticipantId = exports.LogicId = exports.TagParticipantV0 = exports.TagLogicIdV0 = exports.TagAssetV0 = exports.IdentifierTag = exports.IdentifierKind = exports.Identifier = exports.setFlag = exports.getFlag = exports.Flag = exports.AssetId = void 0;
+exports.ParticipantId = exports.LogicId = exports.TagParticipantV0 = exports.TagLogicV0 = exports.TagAssetV0 = exports.IdentifierTag = exports.IdentifierKind = exports.Identifier = exports.Flag = exports.AssetId = void 0;
 var asset_id_1 = require("./asset-id");
 Object.defineProperty(exports, "AssetId", { enumerable: true, get: function () { return asset_id_1.AssetId; } });
 var flags_1 = require("./flags");
 Object.defineProperty(exports, "Flag", { enumerable: true, get: function () { return flags_1.Flag; } });
-Object.defineProperty(exports, "getFlag", { enumerable: true, get: function () { return flags_1.getFlag; } });
-Object.defineProperty(exports, "setFlag", { enumerable: true, get: function () { return flags_1.setFlag; } });
 var identifier_1 = require("./identifier");
 Object.defineProperty(exports, "Identifier", { enumerable: true, get: function () { return identifier_1.Identifier; } });
 var identifier_kind_1 = require("./identifier-kind");
@@ -14,7 +12,7 @@ Object.defineProperty(exports, "IdentifierKind", { enumerable: true, get: functi
 var identifier_tag_1 = require("./identifier-tag");
 Object.defineProperty(exports, "IdentifierTag", { enumerable: true, get: function () { return identifier_tag_1.IdentifierTag; } });
 Object.defineProperty(exports, "TagAssetV0", { enumerable: true, get: function () { return identifier_tag_1.TagAssetV0; } });
-Object.defineProperty(exports, "TagLogicIdV0", { enumerable: true, get: function () { return identifier_tag_1.TagLogicV0; } });
+Object.defineProperty(exports, "TagLogicV0", { enumerable: true, get: function () { return identifier_tag_1.TagLogicV0; } });
 Object.defineProperty(exports, "TagParticipantV0", { enumerable: true, get: function () { return identifier_tag_1.TagParticipantV0; } });
 var logic_id_1 = require("./logic-id");
 Object.defineProperty(exports, "LogicId", { enumerable: true, get: function () { return logic_id_1.LogicId; } });
--- packages/js-moi-identifiers/src.ts/index.ts
+++ packages/js-moi-identifiers/src.ts/index.ts
@@ -1,7 +1,7 @@
 export { AssetId } from "./asset-id";
-export { Flag, getFlag, setFlag } from "./flags";
+export { Flag } from "./flags";
 export { Identifier } from "./identifier";
 export { IdentifierKind } from "./identifier-kind";
-export { IdentifierTag, TagAssetV0, TagLogicV0 as TagLogicIdV0, TagParticipantV0 } from "./identifier-tag";
+export { IdentifierTag, TagAssetV0, TagLogicV0, TagParticipantV0 } from "./identifier-tag";
 export { LogicId } from "./logic-id";
 export { ParticipantId } from "./participant-id";
