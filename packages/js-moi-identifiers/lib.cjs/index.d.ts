020e44ac09654a73ed2e1f3a2a001f386d43730d
Harsh Rastogi <harsh.rastogi@sarva.ai>
2025-01-30 10:45:48 +0530

    chore: remove obsolete TypeScript declaration files and configuration for js-moi-identifiers package

--- packages/js-moi-identifiers/lib.cjs/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { AssetId } from "./asset-id";
-export { IdentifierKind, IdentifierVersion } from "./enums";
-export { Flag } from "./flags";
-export { Identifier, isIdentifier, type InvalidReason } from "./identifier";
-export { IdentifierTag } from "./identifier-tag";
-export { LogicId } from "./logic-id";
-export { createParticipantId, ParticipantId, type GenerateParticipantOption } from "./participant-id";
-//# sourceMappingURL=index.d.ts.map
--- packages/js-moi-identifiers/lib.esm/index.js
+++
@@ -1,8 +0,0 @@
-export { AssetId } from "./asset-id";
-export { IdentifierKind, IdentifierVersion } from "./enums";
-export { Flag } from "./flags";
-export { Identifier, isIdentifier } from "./identifier";
-export { IdentifierTag } from "./identifier-tag";
-export { LogicId } from "./logic-id";
-export { createParticipantId, ParticipantId } from "./participant-id";
-//# sourceMappingURL=index.js.map
