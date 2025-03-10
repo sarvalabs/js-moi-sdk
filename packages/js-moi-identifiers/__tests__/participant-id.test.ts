f9a7c02f24f12132ecbd5a3a9b254d2192bf5042
Harsh Rastogi <rastogiharsh04@gmail.com>
2025-03-10 12:08:10 +0530

    test: rename asset ID constants for clarity in tests

--- packages/js-moi-identifiers/__tests__/participant-id.test.ts
+++ packages/js-moi-identifiers/__tests__/participant-id.test.ts
@@ -2,16 +2,16 @@ import { createParticipantId, Flag, Identifier, IdentifierKind, IdentifierVersio
 import { hexToBytes } from "../src.ts/utils";
 
 const VALID_PARTICIPANT_ID = "0x0000000067bc504a470c5e31586eeedbefe73ccef20e0a49e1dc75ed00000000";
-const NOT_A_PARTICIPANT_ID = "0x208300005edd2b54c4b613883b3eaf5d52d22d185e1d001a023e3f7800000000";
+const INVALID_PARTICIPANT_ID = "0x208300005edd2b54c4b613883b3eaf5d52d22d185e1d001a023e3f7800000000";
 
 describe(ParticipantId, () => {
     describe("constructor", () => {
         it.concurrent("should throw error for invalid participant id", () => {
-            expect(() => new ParticipantId(NOT_A_PARTICIPANT_ID)).toThrow();
+            expect(() => new ParticipantId(INVALID_PARTICIPANT_ID)).toThrow();
         });
 
         it.concurrent("should throw error if identifier kind is not participant", () => {
-            expect(() => new ParticipantId(new Identifier(NOT_A_PARTICIPANT_ID))).toThrow();
+            expect(() => new ParticipantId(new Identifier(INVALID_PARTICIPANT_ID))).toThrow();
         });
 
         it.concurrent("should create a new participant id", () => {
@@ -26,7 +26,7 @@ describe(ParticipantId, () => {
 
     describe(ParticipantId.validate, () => {
         it.concurrent("should invalid result for invalid participant id", () => {
-            expect(ParticipantId.validate(NOT_A_PARTICIPANT_ID)).not.toBeNull();
+            expect(ParticipantId.validate(INVALID_PARTICIPANT_ID)).not.toBeNull();
         });
 
         it.concurrent("should return null for valid participant id", () => {
