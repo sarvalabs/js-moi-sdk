e08daf08455f539a29d7fe280098d11f5bfd5f33
Harsh Rastogi <harsh.rastogi@sarva.ai>
2025-03-28 10:43:42 +0530

    refactor: simplify RoutineCallback type definition for improved readability

--- packages/js-moi-logic/src.ts/logic-driver.ts
+++ packages/js-moi-logic/src.ts/logic-driver.ts
@@ -225,7 +225,7 @@ export class LogicDriver<TRoutines extends LogicRoutines = LogicRoutines> extend
     private newRoutine(routine: string) {
         const isDeployerRoutine = this.getRoutineType(routine) === RoutineType.Deploy;
 
-        const callback: RoutineCallback = async (...args: unknown[]) => {
+        const callback = async (...args) => {
             const isDeployed = await this.isDeployed();
 
             if (isDeployerRoutine && isDeployed) {
@@ -266,7 +266,7 @@ export class LogicDriver<TRoutines extends LogicRoutines = LogicRoutines> extend
             return response;
         };
 
-        return callback;
+        return callback as RoutineCallback;
     }
 
     private setupEndpoint() {
