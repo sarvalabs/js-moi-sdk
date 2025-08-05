70d6e9184decd0c0784056f723f1a0922d568f36
Gokul R <gokul@sarva.ai>
2025-08-05 15:10:48 +0530

    release: bump the package version to v0.6.1-rc1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,9 @@ author = 'Sarva Labs Inc'
 # built documents.
 #
 # The short X.Y version.
-version = u'v0.6.0'
+version = u'v0.6.1-rc1'
 # The full version, including alpha/beta/rc tags.
-release = u'v0.6.0'
+release = u'v0.6.1-rc1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- packages/js-moi-constants/lib.esm/version.d.ts
+++ packages/js-moi-constants/lib.esm/version.d.ts
@@ -1,2 +1,2 @@
-export declare const VERSION = "0.6.0";
+export declare const VERSION = "0.6.1-rc1";
 //# sourceMappingURL=version.d.ts.map
