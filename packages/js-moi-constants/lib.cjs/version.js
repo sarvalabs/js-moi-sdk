cd0f2773d41feeace8d986db06093b582f5da997
Gokul R <gokul@sarva.ai>
2025-08-06 10:36:28 +0530

    fix: resolved logic driver output decode bug

--- packages/js-moi-constants/lib.cjs/version.js
+++ packages/js-moi-constants/lib.cjs/version.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VERSION = void 0;
-exports.VERSION = "0.6.1-rc1";
+exports.VERSION = "0.6.1-rc2";
 //# sourceMappingURL=version.js.map
--- packages/js-moi-constants/lib.esm/version.js
+++ packages/js-moi-constants/lib.esm/version.js
@@ -1,2 +1,2 @@
-export const VERSION = "0.6.1-rc1";
+export const VERSION = "0.6.1-rc2";
 //# sourceMappingURL=version.js.map
