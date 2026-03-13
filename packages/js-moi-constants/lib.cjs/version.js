6a6cb66678ab6163be26b9ca9ad33bc60432be21
Gokul R <gokul@sarva.ai>
2026-03-13 16:46:33 +0530

    bump v0.7.0-rc8 to v0.7.0-rc9

--- packages/js-moi-constants/lib.cjs/version.js
+++ packages/js-moi-constants/lib.cjs/version.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VERSION = void 0;
-exports.VERSION = "0.7.0-rc8";
+exports.VERSION = "0.7.0-rc9";
 //# sourceMappingURL=version.js.map
