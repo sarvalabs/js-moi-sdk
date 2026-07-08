345789bfcc986d2744796706b09813a365f01729
Gokul R <gokul@sarva.ai>
2026-07-08 08:48:24 +0530

    chore: bump the package version to v0.7.1

--- packages/js-moi-constants/lib.esm/version.d.ts
+++ packages/js-moi-constants/lib.esm/version.d.ts
@@ -1,2 +1,2 @@
-export declare const VERSION = "0.7.0";
+export declare const VERSION = "0.7.1";
 //# sourceMappingURL=version.d.ts.map
--- packages/js-moi-constants/lib.esm/version.js
+++ packages/js-moi-constants/lib.esm/version.js
@@ -1,2 +1,2 @@
-export const VERSION = "0.7.0";
+export const VERSION = "0.7.1";
 //# sourceMappingURL=version.js.map
